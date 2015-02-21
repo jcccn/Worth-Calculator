685f9a57f1eae589e2d63f7a6dc848e605d876b4
jcccn <jccuestc@gmail.com>
2015-02-21 19:20:56 +0800

    新建工程

---
+++ WorthCal/WorthCal/ViewControllers/BaseViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BaseViewController.swift
+//  WorthCal
+//
+//  Created by Jiang Chuncheng on 2/20/15.
+//  Copyright (c) 2015 SenseForce. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ WorthCal/WorthCal/ViewControllers/WorthViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WorthViewController.swift
+//  WorthCal
+//
+//  Created by Jiang Chuncheng on 2/20/15.
+//  Copyright (c) 2015 SenseForce. All rights reserved.
+//
+
+import UIKit
+
+class WorthViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "身价计算器";
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
